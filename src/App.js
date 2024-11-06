5d43c02ffe4f24db6a63e6336701d81cdd58c10e
jery koupun <s22129077@student.unklab.ac.id>
2024-11-06 12:42:45 +0800

    Adding initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Header from "./Components/Header";
+import Hero from "./Components/Hero";
+import Clients from "./Components/Clients";
+import Features from "./Components/Features";
+import Pricing from "./Components/Pricing";
+import Footer from "./Components/Footer";
+
+const App = () => {
+  return (
+    <div className="body-wrap">
+      <Header />
+      <main className="site-content">
+        <Hero />
+        <Clients />
+        <Features />
+        <Pricing />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
